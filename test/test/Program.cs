355aa206da6d3d6bddd23dcf4386ce83fe0acab2
Cameron Allkin <cameron.robert.allkin@gmail.com>
2023-07-06 12:08:54 +1000

    update

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,9 +1,40 @@
-﻿namespace Test;
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
 
-internal class Program
+namespace Test;
+
+internal class Display
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("test");
+        string[] lines = File.ReadAllLines(args[0].ToString());
+        Array.Sort(lines, customSort);
+
+        for(int i=0;  i<lines.Length; i++)
+        {
+            Console.WriteLine(lines[i]);
+        }
+
+        File.WriteAllLines(@"sorted.txt", lines);
+    }
+
+    static int customSort(string a, string b)
+    {
+        string[] aArray = a.Split(' ');
+        string[] bArray = b.Split(' ');
+        string aLN = aArray[aArray.Length-1];
+        string bLN = bArray[bArray.Length-1];
+
+        int LNC =  string.Compare(aLN, bLN);
+
+        if(LNC == 0)
+        {
+            return string.Compare(a, b);
+        }
+        else
+        {
+            return LNC;
+        }
     }
 }
